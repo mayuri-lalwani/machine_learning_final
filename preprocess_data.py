82380e47f3393e802d5e87658010fa909542eefc
Yuxing Wang <wanggyux@gmail.com>
2021-05-23 11:25:35 -0700

    preprocess_data module

--- preprocess_data.py
+++ preprocess_data.py
@@ -6,7 +6,7 @@ def one_hot_encoding_labels(bridge_df, _featurre):
     # creating instance of one-hot-encoder
     enc = OneHotEncoder(handle_unknown='ignore')
     # passing bridge-types-cat column (label encoded values of bridge_types)
-    enc_df = pd.DataFrame(enc.fit_transform(bridge_df[[_featurre]]).toarray())
+    enc_df = pd.DataFrame(enc.fit_transform(bridge_df[[_featurre]]).toarray(), columns=enc.categories_)
     # merge with main df bridge_df on key values
     bridge_df = bridge_df.join(enc_df)
     return bridge_df
