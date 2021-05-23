a9a7aadf9ba0f903a8aae87545b50b3d243e2060
Yuxing Wang <wanggyux@gmail.com>
2021-05-23 12:15:20 -0700

    update with valid labeling

--- load_data.py
+++ load_data.py
@@ -3,3 +3,10 @@ import pandas as pd
 def load_potitifact_data():
     df = pd.read_csv('./dataset/politifact_dataset.csv')
     return df
+
+def remove_invalid_verdict(df):
+    df = df[df.Verdict != 'half-flip']
+    df = df[df.Verdict != 'full-flop']
+    df = df[df.Verdict != 'no-flip']
+    print(df.Verdict.unique())
+    return df
--- preprocess_data.py
+++ preprocess_data.py
@@ -2,11 +2,13 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 
-def one_hot_encoding_labels(bridge_df, _featurre):
+def one_hot_encoding_labels(bridge_df, _feature):
     # creating instance of one-hot-encoder
     enc = OneHotEncoder(handle_unknown='ignore')
     # passing bridge-types-cat column (label encoded values of bridge_types)
-    enc_df = pd.DataFrame(enc.fit_transform(bridge_df[[_featurre]]).toarray(), columns=enc.categories_)
+    results = enc.fit_transform(bridge_df[[_feature]]).toarray()
+    enc_df = pd.DataFrame(results, columns=enc.categories_)
+    print(enc.categories_)
     # merge with main df bridge_df on key values
     bridge_df = bridge_df.join(enc_df)
     return bridge_df
