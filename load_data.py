06cfb0f97808d5c156cb2b02028c6fbc1ac21a27
Yuxing Wang <wanggyux@gmail.com>
2021-05-24 12:32:29 -0700

    methods for load second and augument datasets

--- load_data.py
+++ load_data.py
@@ -10,3 +10,16 @@ def remove_invalid_verdict(df):
     df = df[df.Verdict != 'no-flip']
     print(df.Verdict.unique())
     return df
+
+def load_africacheck_data():
+    df = pd.read_csv('./dataset/africa_check_dataset.csv')
+    return df
+
+def load_augument_data():
+    df1 = load_potitifact_data()
+    df2 = load_africacheck_data()
+    _features =['Statement', 'Verdict']
+    df1 = df1[_features]
+    df2 = df2[_features]
+    df = df1.merge(df2, how='left', on=_features)
+    return df
