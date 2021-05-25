c38494e4f559ef45026dec668a9d7d318e203fbf
Yuxing Wang <wanggyux@gmail.com>
2021-05-24 18:21:50 -0700

    second dataset label normalization

--- load_data.py
+++ load_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import requests
 
-def load_potitifact_data():
+def c():
     df = pd.read_csv('./dataset/politifact_dataset.csv')
     return df
 
@@ -14,6 +14,23 @@ def remove_invalid_verdict(df):
 
 def load_africacheck_data():
     df = pd.read_csv('./dataset/africa_check_dataset.csv')
+    df['Verdict'] = df['Verdict'].str.lower()
+    trans_dict ={
+        'checked': 'mostly-true',
+        'unproven': 'false',
+        'incorrect': 'pants-fire',
+        'misleading': 'barely-true',
+        'mostly correct': 'mostly-true',
+        'correct': 'true',
+        'understated': 'half-true',
+        'exaggerated': 'half-true',
+        'downplayed': 'half-true',
+        'mostly-correct': 'mostly-true',
+        'verdict': 'true',
+        'name-field-verdict': 'mostly-true',
+        'not-rated': 'false'
+    }
+    df.replace({'Verdict': trans_dict}, inplace=True)
     return df
 
 def load_augument_data():
@@ -21,8 +38,13 @@ def load_augument_data():
     df2 = load_africacheck_data()
     _features =['Statement', 'Verdict']
     df1 = df1[_features]
+    print(df1.head())
+    print(df1.size)
     df2 = df2[_features]
+    print(df2.head())
+    print(df2.size)
     df = df1.merge(df2, how='left', on=_features)
+    df.to_csv('dataset/df_merge.csv')
     return df
 
 def scrape_data_from_newsapi():
